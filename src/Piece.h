570108e7697df327685a6b09305aa938770dd275
Rishika Saria <rishikasaria@Rishikas-MacBook-Air.local>
2024-08-28 13:32:04 +0530

    added structure of piece and block and some utility functions

---
+++ src/Piece.h
@@ -0,0 +1,25 @@
+#ifndef BITTORRENTCLIENT_PIECE_H
+#define BITTORRENTCLIENT_PIECE_H
+
+#include "Block.h"
+
+class Piece
+{
+private:
+    const std::string hashValue;
+
+public:
+    const int index;
+    std::vector<Block*> blocks;
+
+    explicit Piece(int index, std::vector<Block*> blocks, std::string hashValue);
+    ~Piece();
+    void reset();
+    std::string getData();
+    Block* nextRequest();
+    void blockReceived(int offset, std::string data);
+    bool isComplete();
+    bool isHashMatching();
+};
+
+#endif //BITTORRENTCLIENT_PIECE_H
